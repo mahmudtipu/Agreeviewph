dd48cf3e12abe43073a4a8bb33dfd364533dec87
mahmudtipu <apelmahmudtipu@gmail.com>
2021-04-26 07:33:31 +0600

    source code

---
+++ app/src/main/java/com/agreviewph/onlinequizapp/Common/Common.java
@@ -0,0 +1,15 @@
+package com.agreviewph.onlinequizapp.Common;
+
+import com.agreviewph.onlinequizapp.Model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static String categoryId;
+    public static List<Question> questionList = new ArrayList<>();
+    public static String QuesCategoryIndex;
+    public static int lol=0;
+    public static int totalAdTap=0;
+    public static int videoAdTap=0;
+}
---
+++ app/src/main/java/com/agreviewph/onlinequizapp/Model/Category.java
@@ -0,0 +1,20 @@
+package com.agreviewph.onlinequizapp.Model;
+
+public class Category {
+    private String Name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
